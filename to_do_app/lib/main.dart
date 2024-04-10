bc2e8cb1ae7fc6a3523ac251a1d3e2c77e7968fc
samp000 <sandeshdmarathe333@gmail.com>
2024-04-10 20:31:23 +0530

    assets added

--- to_do_app/lib/main.dart
+++ to_do_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:to_do_app/loginPage.dart';
+import 'package:to_do_app/AdvanceToDo/loginPage.dart';
+// import 'package:to_do_app/BasicToDo/loginPage.dart';
 
 void main() async {
   runApp(const MainApp());
